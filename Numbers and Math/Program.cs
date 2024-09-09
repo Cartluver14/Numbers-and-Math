42db4876918b7eb8ff639b15119c780b52eb181c
North Harrington <339713067@GoTVDSB.ca>
2024-09-09 09:12:29 -0400

    Add project files.

---
+++ Numbers and Math/Program.cs
@@ -0,0 +1,10 @@
+﻿//North
+using System.ComponentModel;
+
+Console.WriteLine("My age is");
+Console.WriteLine(2024 - 2008);
+Console.WriteLine("Press ENTER to continue:");
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine("What is the average"); 
+Console.WriteLine((14 + 15 + 16) / 3); 
